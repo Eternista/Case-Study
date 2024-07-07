b125d65113c7f4c04a77d36639e50f5252111d33
Eternista <rafal.ziolkowski1234@gmail.com>
2024-07-07 20:45:36 +0200

    MAIN: BEGINING

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/sass/style.scss';
+import Header from './components/partials/Header';
+import Main from './components/partials/Main';
+import Footer from './components/partials/Footer';
+import { useState } from 'react';
+
+function App() {
+  const [isLogged, setIsLogged] = useState<boolean>(true);
+  return isLogged === true ? (
+        <>
+          <Header/>
+          <Main/>
+          <Footer/>
+        </>
+
+    ) : (
+      <>
+        Nope
+      </>
+    )
+}
+
+export default App
