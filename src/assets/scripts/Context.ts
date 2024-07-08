5bc0ad60551ad87732daf1ccf0e94eb53c3eb5c8
Eternista <rafal.ziolkowski1234@gmail.com>
2024-07-08 23:29:04 +0200

    ASSETS: updated assets files and scss code

---
+++ src/assets/scripts/Context.ts
@@ -0,0 +1,39 @@
+export const PageTranscripts = {
+    intro: {
+        title: "Mobile Design System",
+        description: "The Mobile Design System (MDS) project for Nissan aimed to create a cohesive and accessible design system from scratch, standardizing the style across all Nissan mobile interfaces globally.",
+        information: {
+            company: 'Nissan',
+            role: "UX/UI Designer",
+            collaboration: "Worked as part of a team",
+            status: 'Completed'
+        }
+    },
+    challenge: {
+        title: "Challenge",
+        texts: [
+            "<p>- Led the effort to unify Nissan’s brand identity across global mobile platforms through the implementation of the Mobile Design System (MDS).</p>",
+            "<p>- Ensured the Mobile Design System (MDS) reflected and extended the design principles established in the (WDS) Web Design System.</p>"
+        ]
+    },
+    responsibilites: {
+        title: "Responsibilites",
+        texts: [
+            "<p>As a UX/UI Designer at Nissan, I collaborated closely with team to develop and implement the Mobile Design System (MDS), focusing on enhancing usability and brand coherence.</p>",
+            "<p>- Contributed to the design of the Mobile Design System (MDS), encompassing typography, color schemes, atomic and molecular components.</p>",
+            "<p>- Collaborated with stakeholders and team members to define system architecture and ensure adherence to Nissan’s design principles and accessibility guidelines.</p>"
+        ]
+    },
+    result: {
+        title: "Results and Achievements",
+        texts: [
+            "<p>The Mobile Design System (MDS) enhanced Nissan’s brand consistency and user experience globally by providing a scalable and accessible framework, positively impacting usability metrics and stakeholder feedback.</p>"
+        ]
+    },
+    conclusion: {
+        title: "Conclusion",   
+        texts:[
+            "<p>“The Mobile Design System (MDS) project for Nissan provided valuable insights into the complexities of aligning global brand standards with user-centric mobile interface design. This experience strengthened my proficiency in collaborative design processes, emphasizing the importance of iterative development and stakeholder engagement in achieving cohesive and effective design solutions.”</p>"
+        ],
+    }
+}
